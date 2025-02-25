66a62214bf07e27c00af639017edf22a215576e0
Stanislav Shandyga <stanislavshandyga@gmail.com>
2025-02-25 19:08:52 +0200

    Update cli script

--- remarks.ps1
+++ remarks.ps1
@@ -2,27 +2,61 @@ param(
     $CommandName
 )
 
-$command = "cargo run -q -p canopus-cli -- "
+$base_command = "cargo run -q -p canopus-cli -- "
+
+function LoadNextPage {
+    Write-Host "Press Enter to load next page or Esc to exit..."
+
+    while ($true) {
+        $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+        if ($key.VirtualKeyCode -eq 13) {  # Enter key
+            return $true
+        } elseif ($key.VirtualKeyCode -eq 27) {  # Escape key
+            return $false
+        }
+    }
+}
 
 switch ($CommandName) {
     "delete" {
         $id = Read-Host 'Remark ID'
-        $command = $command + "Delete-Remark --ID $id"
+        $command = $base_command + "Delete-Remark --ID $id"
+
+        Invoke-Expression $command
     }
 
     "get" {
         $id = Read-Host 'Remark ID'
-        $command = $command + "Get-Remark --ID $id" + " | ConvertFrom-Json"
+        $command = $base_command + "Get-Remark --ID $id" + " | ConvertFrom-Json"
+
+        Invoke-Expression $command
     }
 
     "list" {
-        $command = $command + "List-Remarks" + " | ConvertFrom-Csv"
-        Write-Host $command
+        $command = $base_command + "List-Remarks"
+        $listing_command = $command + " | ConvertFrom-Csv"
+        $rows = Invoke-Expression $listing_command
+
+        do {
+            if ($rows.Count -eq 0) {
+                return
+            }
+
+            $rows | Format-Table
+
+            if (-not(LoadNextPage)) {
+                return
+            }
+
+            $listing_command = $command + " --LoadNextPage | ConvertFrom-Csv"
+            $rows = Invoke-Expression $listing_command
+        } while($true)
     }
 
     "new" {
         $essence = Read-Host 'Remark essence'
-        $command = $command + "New-Remark --Essence $essence"
+        $command = $base_command + "New-Remark --Essence $essence"
 
         do {
             $tag = Read-Host 'Remark tag'
@@ -33,7 +67,9 @@ switch ($CommandName) {
                 break
             }
         } while ($true)
+
+        $new_command = $command + " | ConvertFrom-Json"
+
+        Invoke-Expression $new_command
     }
 }
-
-Invoke-Expression $command
