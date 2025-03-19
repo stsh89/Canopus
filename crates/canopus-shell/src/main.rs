8e54fb28b228a8fbafcb8c0473d6a2fbda7c8a38
Stanislav Shandyga <stanislavshandyga@gmail.com>
2025-03-19 13:06:31 +0200

    [shell] Remove wasted code

--- crates/canopus-shell/src/main.rs
+++ crates/canopus-shell/src/main.rs
@@ -1,107 +1,56 @@
-use canopus_cli::CliState;
-use canopus_definitions::Result;
-use std::io::{Stdin, Stdout};
+use canopus_cli::{Cli, CliContext};
+use eyre::WrapErr;
 
 const BASE_URL: &str = "canopus://127.0.0.1";
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    dotenvy::dotenv().map_err(Into::<eyre::Error>::into)?;
+async fn main() -> eyre::Result<()> {
+    dotenvy::dotenv().wrap_err_with(|| "Failed to load .env file")?;
 
-    let mut exit = false;
-    let mut shell = Shell::start()?;
+    let app = CliContext::initialize().wrap_err_with(|| "Failed to initialize CLI application")?;
 
-    shell.print_welcome_message()?;
+    println!(r#"Enter "help" for usage hints. Enter "quit" or "exit" to close the shell."#);
+    println!();
+    println!();
 
-    while !exit {
-        let input = shell.read()?;
+    loop {
+        let input = read().wrap_err_with(|| "Failed to read user input")?;
+        let input = input.trim();
 
-        match shell.evaluate(input.trim()).await {
-            Ok(is_exit) => exit = is_exit,
-            Err(err) => print_error(&mut shell.stdout, err)?,
-        }
-    }
-
-    Ok(())
-}
-
-struct Shell {
-    state: CliState,
-    stdout: Stdout,
-    stdin: Stdin,
-}
-
-impl Shell {
-    async fn evaluate(&mut self, input: &str) -> Result<bool> {
         if input == "quit" || input == "exit" {
-            return Ok(true);
+            return Ok(());
         }
 
         let mut args: Vec<&str> = input.split_whitespace().collect();
         args.insert(0, "");
 
-        canopus_cli::run_from(&self.state, args).await?;
-
-        Ok(false)
-    }
-
-    fn prompt(&self) -> String {
-        format!("{}> ", BASE_URL)
-    }
-
-    fn print_welcome_message(&mut self) -> Result<()> {
-        let message = r#"Enter "help" for usage hints. Enter "quit" or "exit" to close the shell."#;
-
-        print_message(&mut self.stdout, message)?;
-
-        println!();
-        println!();
-
-        Ok(())
-    }
-
-    fn start() -> Result<Self> {
-        Ok(Self {
-            stdin: std::io::stdin(),
-            stdout: std::io::stdout(),
-            state: CliState::new()?,
-        })
-    }
-
-    fn read(&mut self) -> Result<String> {
-        use std::io::Write;
-
-        self.stdout
-            .write_all(self.prompt().as_bytes())
-            .map_err(Into::<eyre::Error>::into)?;
-
-        self.stdout.flush().map_err(Into::<eyre::Error>::into)?;
-
-        let mut buffer = String::new();
-
-        if let Err(err) = self.stdin.read_line(&mut buffer) {
-            return Err(eyre::Error::from(err).into());
+        let cli = match Cli::new_with_args(&args) {
+            Ok(cli) => cli,
+            Err(err) => {
+                eprintln!("{err}");
+                continue;
+            }
         };
 
-        Ok(buffer)
+        if let Err(err) = app.execute(cli).await {
+            eprintln!("{}", err);
+        }
     }
 }
 
-fn print_error(stdout: &mut Stdout, error: impl std::error::Error) -> Result<()> {
-    print_message(stdout, error)?;
-    println!();
-
-    Ok(())
+fn prompt() -> String {
+    format!("{}> ", BASE_URL)
 }
 
-fn print_message(stdout: &mut Stdout, message: impl std::fmt::Display) -> Result<()> {
+fn read() -> eyre::Result<String> {
     use std::io::Write;
 
-    let message = format!("{}", message);
+    std::io::stdout().write_all(prompt().as_bytes())?;
+    std::io::stdout().flush()?;
 
-    stdout
-        .write_all(message.as_bytes())
-        .map_err(Into::<eyre::Error>::into)?;
+    let mut buffer = String::new();
 
-    Ok(())
+    std::io::stdin().read_line(&mut buffer)?;
+
+    Ok(buffer)
 }
